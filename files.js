bc93797953157e3af0b0ee4879697ece216f53d1
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-08 17:23:06 -1000

    resolves crashing when requesting unknown files

--- files.js
+++ files.js
@@ -20,7 +20,7 @@ module.exports = {
     </ol>
   </body>
   </html>`,
-  "index.html": `<!DOCTYPE html>
+  "/index.html": `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -41,7 +41,7 @@ module.exports = {
   </ol>
 </body>
 </html>`,
-  "404.html": `<!DOCTYPE html>
+  "/404.html": `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -56,7 +56,7 @@ module.exports = {
   </p>
 </body>
 </html>`,
-  "helium.html": `<!DOCTYPE html>
+  "/helium.html": `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -71,7 +71,7 @@ module.exports = {
   <p><a href="/">back</a></p>
 </body>
 </html>`,
-  "hydrogen.html": `<!DOCTYPE html>
+  "/hydrogen.html": `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -85,5 +85,115 @@ module.exports = {
   <p>Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the universe, constituting roughly 75% of all baryonic mass. Non-remnant stars are mainly composed of hydrogen in its plasma state. The most common isotope of hydrogen, termed protium (name rarely used, symbol 1H), has a single proton and zero neutrons.</p>
   <p><a href="/">back</a></p>
 </body>
-</html>`
+</html>`,
+  "/css/styles.css": `@import url(http://fonts.googleapis.com/css?family=Open+Sans|Roboto+Slab);
+
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+/* STYLES */
+
+body{
+  background-color: #3F3F4E;
+  font-family: 'Open Sans', 'Helvetica', sans-serif;
+  padding: 50px;
+  max-width: 500px;
+  margin: auto;
+}
+
+h1, h2, h3, h4, h5, h6{
+  font-family: 'Roboto Slab', 'Helvetica', sans-serif;
+  color: #B4D12B;
+  margin-bottom: 20px;
+}
+
+h1{
+  font-size: 50px;
+  line-height: 55px;
+}
+
+h2{
+  font-size: 30px;
+  line-height: 38px;
+  color: #879642;
+}
+
+h3{
+  font-size: 25px;
+  line-height: 30px;
+  color: #F2FFC1;
+}
+
+p{
+  color: #C6C5AC;
+  font-family: 'Open Sans', 'Helvetica', sans-serif;
+  line-height: 26px;
+  font-size: 15px;
+}
+
+ul{
+
+}
+
+li{
+  line-height: 26px;
+  font-size: 15px;
+}
+
+a{
+  color: #F2FFC1;
+  text-decoration: none;
+  border-bottom: 1px dashed #E3DE8B;
+}
+
+a:hover{
+  color: #C6C5AC;
+  border-bottom: 1px dashed #C6C5AC;
+}`
 };
--- server.js
+++ server.js
@@ -6,30 +6,40 @@ const PORT = 8080;
 const server = net.createServer(socket => {
   socket.on("data", chunk => {
     // read incoming data
-
-    console.log("data");
-    console.log(chunk.toString());
-    console.log("foo");
+    // console.log("foo");
+    // console.log(chunk.toString());
 
     // parse the string
     // - isolate the part between the spaces
-    console.log(chunk.toString().split(" ")[1]);
+    // console.log(chunk.toString().split(" ")[1]);
 
     // grab the right file
-
-    console.log(files[chunk.toString().split(" ")[1]]);
+    // console.log(files[chunk.toString().split(" ")[1]]);
 
     // write outgoing data
+    // - check content-length
+    // console.log(chunk.toString());
+    // console.log(chunk.toString().split(" ")[1]);
+    // console.log(files[chunk.toString().split(" ")[1]].length);
+    if (chunk.toString().split(" ")[1] in files) {
+      socket.write(`HTTP/1.1 200 OK
+Server: Rian's custom JS HTTP server
+Date: ${new Date().toUTCString()}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${files[chunk.toString().split(" ")[1]].length}
+Connection: keep-alive
 
-    socket.write(`HTTP/1.1 200 OK
-    Server: nginx/1.4.6 (Ubuntu)
-    Date: ${new Date().toUTCString()}
-    Content-Type: text/html; charset=utf-8
-    Content-Length: ${files[chunk.toString().split(" ")[1]].length}
-    Connection: keep-alive
+${files[chunk.toString().split(" ")[1]]}`);
+    } else {
+      socket.write(`HTTP/1.1 404 Not Found
+Server: Rian's custom JS HTTP server
+Date: ${new Date().toUTCString()}
+Content-Type: text/html; charset=utf-8
+Content-Length: 263
+Connection: keep-alive
 
-    ${files[chunk.toString().split(" ")[1]]}
-    `);
+${files["/404.html"]}`);
+    }
     socket.end();
   });
 

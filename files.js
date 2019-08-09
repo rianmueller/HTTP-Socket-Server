1fe2d638a7abd7564ba9e49d959da0627592d62e
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-08 16:21:09 -1000

    transmits HTML body for /

---
+++ files.js
@@ -0,0 +1,89 @@
+module.exports = {
+  "/": `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>The Elements</title>
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+  <body>
+    <h1>The Elements</h1>
+    <h2>These are all the known elements.</h2>
+    <h3>These are 2</h3>
+    <ol>
+      <li>
+        <a href="/hydrogen.html">Hydrogen</a>
+      </li>
+      <li>
+        <a href="/helium.html">Helium</a>
+      </li>
+    </ol>
+  </body>
+  </html>`,
+  "index.html": `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>The Elements</h1>
+  <h2>These are all the known elements.</h2>
+  <h3>These are 2</h3>
+  <ol>
+    <li>
+      <a href="/hydrogen.html">Hydrogen</a>
+    </li>
+    <li>
+      <a href="/helium.html">Helium</a>
+    </li>
+  </ol>
+</body>
+</html>`,
+  "404.html": `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Element not found!</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>404</h1>
+  <h2>Element not found!</h2>
+  <p>
+    <a href="/">back</a>
+  </p>
+</body>
+</html>`,
+  "helium.html": `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - Helium</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>Helium</h1>
+  <h2>H</h2>
+  <h3>Atomic number 2</h3>
+  <p>Helium is a chemical element with symbol He and atomic number 2. It is a colorless, odorless, tasteless, non-toxic, inert, monatomic gas that heads the noble gas group in the periodic table. Its boiling and melting points are the lowest among all the elements and it exists only as a gas except in extremely cold conditions.</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`,
+  "hydrogen.html": `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - Hydrogen</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>Hydrogen</h1>
+  <h2>H</h2>
+  <h3>Atomic number 1</h3>
+  <p>Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the universe, constituting roughly 75% of all baryonic mass. Non-remnant stars are mainly composed of hydrogen in its plasma state. The most common isotope of hydrogen, termed protium (name rarely used, symbol 1H), has a single proton and zero neutrons.</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`
+};
--- server.js
+++ server.js
@@ -6,15 +6,30 @@ const PORT = 8080;
 const server = net.createServer(socket => {
   socket.on("data", chunk => {
     // read incoming data
+
     console.log("data");
     console.log(chunk.toString());
+    console.log("foo");
 
     // parse the string
+    // - isolate the part between the spaces
+    console.log(chunk.toString().split(" ")[1]);
 
     // grab the right file
 
+    console.log(files[chunk.toString().split(" ")[1]]);
+
     // write outgoing data
-    socket.write("test");
+
+    socket.write(`HTTP/1.1 200 OK
+    Server: nginx/1.4.6 (Ubuntu)
+    Date: ${new Date().toUTCString()}
+    Content-Type: text/html; charset=utf-8
+    Content-Length: ${files[chunk.toString().split(" ")[1]].length}
+    Connection: keep-alive
+
+    ${files[chunk.toString().split(" ")[1]]}
+    `);
     socket.end();
   });
 
